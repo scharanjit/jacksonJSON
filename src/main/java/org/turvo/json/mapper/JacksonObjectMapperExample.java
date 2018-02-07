1d62aa81382b7e7c3c16be564558a8e6b55df682
sich6002 <charanjit.singh@imaginea.com>
2018-02-07 12:30:28 +0530

    Addded  commit-1

--- src/main/java/org/turvo/json/mapper/JacksonObjectMapperExample.java
+++ src/main/java/org/turvo/json/mapper/JacksonObjectMapperExample.java
@@ -1,22 +1,17 @@
 package org.turvo.json.mapper;
 
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.turvo.json.model.Address;
 import org.turvo.json.model.Employee;
 
